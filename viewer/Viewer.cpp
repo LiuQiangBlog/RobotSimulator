1f48a40faa3657b420b68eb3c5018a92b5a49d83
liuqiang <liuqiang@paxini.com>
2025-05-09 10:51:55 +0800

    add 3rdparty library imgui && implot && implot3d

---
+++ viewer/Viewer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liuqiang on 25-5-9.
+//
+
+#include "Viewer.h"
---
+++ viewer/Viewer.h
@@ -0,0 +1,26 @@
+//
+// Created by liuqiang on 25-5-9.
+//
+
+#ifndef MUJOCO_VIEWER_H
+#define MUJOCO_VIEWER_H
+
+#include <GLFW/glfw3.h>
+#include <Eigen/Geometry>
+#include <mujoco/mujoco.h>
+#include <imgui.h>
+#include <implot.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <iostream>
+#include <stdexcept>
+#include <any>
+#include <logging.h>
+#include <ImGuizmo.h>
+#include <unordered_set>
+
+class Viewer
+{
+};
+
+#endif // MUJOCO_VIEWER_H
